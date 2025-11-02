cd80a868070351eba73b1ad8949a4d35e5d90a3c
PhanDucThe <maytinh519@gmail.com>
2025-11-03 00:18:25 +0700

    Duc The them trang san pham

--- DA_LTW/Controllers/Customer/HomeCustomerController.cs
+++ DA_LTW/Controllers/Customer/HomeCustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DA_LTW.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,20 @@ namespace DA_LTW.Controllers.Customer
 {
     public class HomeCustomerController : Controller
     {
-        // GET: Home
+
+        private db_cnpmEntities db = new db_cnpmEntities();
+
+        // Action này sẽ hiển thị trang chủ với danh sách sản phẩm
         public ActionResult Index()
         {
-            return View();
+            // Lấy danh sách sản phẩm đang được bán, sắp xếp mới nhất lên đầu
+            var products = db.products
+                             .Where(p => p.is_active == true)
+                             .OrderByDescending(p => p.created_at)
+                             .ToList();
+
+            // Truyền danh sách sản phẩm sang View
+            return View(products);
         }
     }
 }
