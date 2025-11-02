f7fa6498a0e90b6598ea9131c9070fb021bc0846
Vi Do <vidotrong@gmail.com>
2025-11-03 00:22:11 +0700

    Home: create Profile

--- DA_LTW/Controllers/Customer/HomeCustomerController.cs
+++ DA_LTW/Controllers/Customer/HomeCustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DA_LTW.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,15 @@ namespace DA_LTW.Controllers.Customer
     public class HomeCustomerController : Controller
     {
         // GET: Home
+        private db_cnpmEntities data = new db_cnpmEntities();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Profile()
+        {
+            var result = data.users.FirstOrDefault();
+            return View(result);
         }
     }
 }
