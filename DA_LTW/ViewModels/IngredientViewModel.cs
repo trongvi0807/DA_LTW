ca81dbb999228498323ef3d2b0d7ea4b15d2e149
PhanDucThe <maytinh519@gmail.com>
2025-11-03 05:17:44 +0700

    Hoan Thanh

---
+++ DA_LTW/ViewModels/IngredientViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DA_LTW.ViewModels
+{
+    public class IngredientViewModel
+    {
+        public string name { get; set; }
+        public string amount { get; set; }
+        public string unit { get; set; }
+    }
+}
---
+++ DA_LTW/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DA_LTW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DA_LTW.ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        // Thông tin sản phẩm chính
+        public product Product { get; set; }
+
+        // Danh sách các ảnh phụ của sản phẩm
+        public List<product_images> Images { get; set; }
+
+        // Danh sách thành phần đã được giải mã từ JSON
+        public List<IngredientViewModel> IngredientsList { get; set; }
+    }
+}
